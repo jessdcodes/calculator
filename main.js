7552928967d32dc63c8cfae21b948295233ac0d6
jessdcodes <dionisiojessica15@gmail.com>
2022-10-05 22:34:12 +0800

    Add digits and operators listener , display output when digit and operator is pressed

--- main.js
+++ main.js
@@ -27,3 +27,38 @@ function operate(operator, num1, num2){
         divide(num1,num2);
     }
 }
+
+function displayOutput(text) {
+    const output = document.querySelector(".output");
+    output.textContent = text;
+}
+
+function getCurrentOutput(){
+    const output = document.querySelector(".output");
+    return output.textContent;
+}
+
+function addBtnActions() {
+    const digitsBtns = Array.from(document.querySelectorAll('button[data-number]'));
+    const operatorBtns = Array.from(document.querySelectorAll('button[data-operator]'));
+
+    let num1 = 0;
+    let num2 = 0;
+    let currentOperator;
+    digitsBtns.forEach(btn => btn.addEventListener("click", ()=> {
+        displayOutput(getCurrentOutput()+""+btn.textContent);
+    }));
+
+    operatorBtns.forEach(btn => btn.addEventListener("click", ()=> {
+        if(isNaN(getCurrentOutput().substring(-1))){
+            displayOutput(num1+" "+btn.textContent+" ");
+            currentOperator = btn.textContent;
+        } else { 
+            num1 = parseInt(getCurrentOutput());
+            displayOutput(getCurrentOutput()+" "+btn.textContent+" ");
+        }
+    }));
+
+}
+
+addBtnActions();
